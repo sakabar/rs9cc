2a5ad620d813f81c619367cfd39000822d1373c1
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2019-10-12 21:54:37 +0900

    トークナイズを実装

--- shell/test.sh
+++ shell/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -u
 
+set -e
+cargo build && cargo test
+set +e
+
 try() {
     expected="$1"
     input="$2"
@@ -33,5 +37,6 @@ try() {
 try 0 0
 try 42 42
 try 21 "5+20-4"
+try 41 " 12 + 34 - 5 "
 
 echo "OK"
